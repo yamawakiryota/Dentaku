ab60c21b966a054e3929ab968de86211799617da
yamawaki ryota <s1531070@eng.kitakyu-u.ac.jp>
2013-11-21 14:41:36 +0900

    Initial Commit

---
+++ dentakuTests/dentakuTests.h
@@ -0,0 +1,13 @@
+//
+//  dentakuTests.h
+//  dentakuTests
+//
+//  Created by 山脇　僚太 on 13/11/21.
+//  Copyright (c) 2013年 univercity of kitakyu. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface dentakuTests : SenTestCase
+
+@end
---
+++ dentakuTests/dentakuTests.m
@@ -0,0 +1,32 @@
+//
+//  dentakuTests.m
+//  dentakuTests
+//
+//  Created by 山脇　僚太 on 13/11/21.
+//  Copyright (c) 2013年 univercity of kitakyu. All rights reserved.
+//
+
+#import "dentakuTests.h"
+
+@implementation dentakuTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in dentakuTests");
+}
+
+@end
